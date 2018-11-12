9a9bde0b36456c479af1622973ad05336086029d
Olarn U <olarn.u@gmail.com>
2018-11-12 16:40:32 +0700

    เพิ่มเรื่อง mock.Verif().

--- TaxApi/TaxTests/VatCalculatorWithRepoTests.cs
+++ TaxApi/TaxTests/VatCalculatorWithRepoTests.cs
@@ -5,38 +5,40 @@ using TaxLib;
 
 namespace TaxTests
 {
-    public class VatCalculatorWithRepoTests {
-        
+    public class VatCalculatorWithRepoTests
+    {
+
         [Theory]
         [InlineData(100, 7, 107)]
         [InlineData(100, 10, 110)]
-        public void Test_TexController_CalculateVat_with_repo(decimal amount, decimal vatRate, decimal expectedTotalAmount) 
+        public void Test_TexController_CalculateVat_with_repo(decimal amount, decimal vatRate, decimal expectedTotalAmount)
         {
             // Arrange
-            var vatCalculator = new VatCalculator();            
+            var vatCalculator = new VatCalculator();
             var mock = new Mock<IRepository>();
             mock.Setup(r => r.getVatRate()).Returns(vatRate);
             var repo = mock.Object;
             var vatManager = new VatManager(repo, vatCalculator);
-    
+
             // Act
             var actualTotalAmount = vatManager.calculateVat(amount);
 
             // Assert
             Assert.Equal(expectedTotalAmount, actualTotalAmount);
+            mock.Verify(r => r.getVatRate(), Times.Once);
         }
 
         [Theory]
         [InlineData(107, 7, 100)]
         [InlineData(110, 10, 100)]
-        public void Test_TaxController_excludeVat_WithRepo(decimal totalAmount, decimal vatRate, decimal expectedAmount) 
+        public void Test_TaxController_excludeVat_WithRepo(decimal totalAmount, decimal vatRate, decimal expectedAmount)
         {
             // Arrange
-            var vatCalculator = new VatCalculator();            
+            var vatCalculator = new VatCalculator();
             var mock = new Mock<IRepository>();
-            
+
             mock.Setup(r => r.getVatRate()).Returns(vatRate);
-            
+
             var repo = mock.Object;
             var vatManager = new VatManager(repo, vatCalculator);
 
