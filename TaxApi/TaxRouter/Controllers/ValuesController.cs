5e18acf3245b92406999638d49b92144187383ac
Olarn U <olarn.u@gmail.com>
2018-11-16 14:15:40 +0700

    Add Router Controllers to support Vat Calculation API.

--- TaxApi/TaxRouter/Controllers/ValuesController.cs
+++ TaxApi/TaxRouter/Controllers/ValuesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TaxRouter.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/values")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
---
+++ TaxApi/TaxRouter/Controllers/VatControllers.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaxRouter.Controllers
+{
+    [Route("api/vat/include")]
+    [ApiController]
+    public class VatControllers : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<decimal> Get()
+        {
+            return 7;
+        }
+
+        [HttpGet("{amount}")]
+        public ActionResult<string> Get(decimal amount)
+        {
+            return (amount * (decimal)1.07).ToString("#.##");
+        }
+    }
+}
